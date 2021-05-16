38179c98ce901b7b0d94247868da9229b01528dd
chuleza99 <84072313+chuleza99@users.noreply.github.com>
2021-05-16 12:04:41 +0200

    Add files via upload

---
+++ chuleza.java
@@ -0,0 +1,15 @@
+/*
+
+Chuleza Chulumanco Pearl Ndzandezeka
+
+219413797
+
+*/
+
+import javax.swing.*;
+public class chuleza {
+  public static void main(String[] args) {
+  JOptionPane.showMessageDialog(null, "Programming is fun",null, JOptionPane.INFORMATION_MESSAGE);
+   System.exit(0);
+  } 
+}
